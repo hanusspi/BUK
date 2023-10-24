9dff2032ba6fc1189c3d4b1de86f41f60899c32e
hanusspi <95216134+hanusspi@users.noreply.github.com>
2023-10-24 18:54:56 +0200

    added comments

--- 1HA/TouringSim/touring.py
+++ 1HA/TouringSim/touring.py
@@ -1,3 +1,4 @@
+#log band and current symbol
 def print_progress(input_list, index):
     if 0 <= index < len(input_list):
         result = ""
@@ -13,34 +14,43 @@ def print_progress(input_list, index):
         print("Index is out of range.")
 
 class TouringMachine:
+    #create Touring Machine class
     def __init__(self, file_path):
         try:
             with open(file_path, "r") as file:
+                #Read .TM file
                 self.states = file.readline().strip('\n')
                 self.alphabet = list(file.readline().strip('\n'))
                 self.int_alphabet = list(file.readline().strip('\n'))
                 self.begin_state = int(file.readline().strip('\n'))
                 self.end_state = int(file.readline().strip('\n'))
                 self.transitions = {}
-                # Read and process each line in the file
+                # Read and process each statetransition
                 for line in file:
-                    # Do something with each line, for example, print it
+                    # fit the state transition in dictionary entries (tuple of old state and symbol for identifing)
+                    # triple for new state, new symbol and next operation
                     tmp = line.strip("\n").split(" ")
                     self.transitions[int(tmp[0]), tmp[1]]=(int(tmp[2]), tmp[3], tmp[4])  # 'end='' to avoid double spacing (lines already have newline characters)
         except FileNotFoundError:
             print(f"File '{file_path}' not found")
         except Exception as e:
             print(f"An error occurred: {e}")
-    
+    #execute the touring machine
     def execute(self, band):
+        #init values
         s = self.begin_state
         i = 1
         band.insert(0,"B")
         band.append("B")
+        #run until final state is reached
         while(s!=self.end_state):
+            #search fitting transition profile from dict
             t = self.transitions[(s,band[i])]
+            #overwrite band entry
             band[i] = t[1]
+            #update state 
             s = t[0]
+            #translate movint instructions to index changes and increase buffer to left and right if needed
             if(t[2] == 'R'):
                 i += 1
                 if(i>=len(band)):
@@ -55,6 +65,13 @@ class TouringMachine:
         band.pop(0)
         
         return band
+    
+
+        
+             
+
+
+
     
 file_path = input("Enter File Path: ")
 band_input = input("Enter Band: ")
